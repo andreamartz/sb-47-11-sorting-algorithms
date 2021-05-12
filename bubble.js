5755b471734f5d0012971b8ae3b18ba6823c247f
andreamartz <14165788+andreamartz@users.noreply.github.com>
2021-05-12 14:56:44 -0500

    Initial commit

---
+++ bubble.js
@@ -0,0 +1,11 @@
+// Input(s): an array of numbers (arr)
+// Return: the array with values sorted from smallest to largest
+
+// Approach: Sort the array in place. Iterate through the array multiple times, each time checking each number to see if it should switch places with the number after it.
+
+// Strategy:
+// 
+function bubbleSort(arr) {
+}
+
+module.exports = bubbleSort;
