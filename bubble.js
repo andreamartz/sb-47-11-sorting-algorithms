2094c004ea50ebc503241dd6ed52da7cbab186d2
andreamartz <14165788+andreamartz@users.noreply.github.com>
2021-05-16 10:55:03 -0500

    Add bubbleSort function

--- bubble.js
+++ bubble.js
@@ -6,6 +6,22 @@
 // Strategy:
 // 
 function bubbleSort(arr) {
+  let count = 0;
+  for (i = 0; i < arr.length; i++) {
+    // set/reset swapped to be false;
+    let swapped = false;
+    for (j = 0; j < arr.length - i; j++) {
+      if (arr[j] > arr[j + 1]) {
+        // use destructuring to swap
+        [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+        swapped = true;
+      }
+    }
+    if (!swapped) break;
+  }
+  return arr;
 }
+
+console.log(bubbleSort([23,80,27,42,3,9,30]));
 
 module.exports = bubbleSort;
