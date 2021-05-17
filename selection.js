17eecb24c170d0cb53372add1beb785cf6d5153e
andreamartz <14165788+andreamartz@users.noreply.github.com>
2021-05-16 20:00:09 -0500

    Add selection sort function

--- selection.js
+++ selection.js
@@ -1,3 +1,19 @@
-function selectionSort() {}
+function selectionSort(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    let idxOfMin = i;
+    // start j at i + 1 because 
+    //   - the indices less than i have been sorted AND
+    //   - the element at idx i is the one to compare to
+    for (let j = i + 1; j < arr.length; j++) {
+      // if the element at index j is smaller than the one at index smIdx, set smIdx equal to j
+      if (arr[j] < arr[idxOfMin]) idxOfMin = j;
+    }
+    // swap elements if necessary
+    if (i !== idxOfMin) [arr[i], arr[idxOfMin]] = [arr[idxOfMin], arr[i]];
+  }
+  return arr;
+}
+
+console.log(selectionSort([12,16,7,2,19,6,46]));
 
 module.exports = selectionSort;
